f8bb1007ad0af5e6afdddd7a846f4715c2ecf51e
Johann Pistorius <johann.pistorius0@gmail.com>
2018-05-24 14:38:38 +0200

    Add files via upload

--- Fenetre.java
+++ Fenetre.java
@@ -1,10 +1,13 @@
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
@@ -24,13 +27,15 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
 public class Fenetre extends JFrame{
 	private JPanel pane;
-	private JTextField text;
+	private JTextField textLogin;
+	private JTextArea text;
 	private JLabel label;
 	private JButton button;
 	private Concierge c;
@@ -55,19 +60,19 @@ public class Fenetre extends JFrame{
 		c.removeFenetre(this);
 		label = new JLabel();
 		label.setText("Login");
-		text = new JTextField(20);  
+		textLogin = new JTextField(20);
 		this.button=new JButton("Connexion");
 		this.button.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				b = new Bavard(text.getText(),c);
+				b = new Bavard(textLogin.getText(),c);
 				c.addListener(b);
 				b.createPapotageEvent("OnLineBavardEvent", "User "+ b.getLogin()+" has joined the channel!");
 				miseAJour();
 			}
 		});
 		button.setEnabled(false);
-		text.getDocument().addDocumentListener(new DocumentListener() {
+		textLogin.getDocument().addDocumentListener(new DocumentListener() {
 			@Override
 			public void changedUpdate(DocumentEvent e) {
 				changed();
@@ -82,7 +87,7 @@ public class Fenetre extends JFrame{
 				changed();
 			}
 			public void changed() {
-			     if (text.getText().length()<3){
+			     if (textLogin.getText().length()<3){
 			         button.setEnabled(false);
 			       }
 			       else {
@@ -91,7 +96,7 @@ public class Fenetre extends JFrame{
 			}
 		});	
 		this.pane.add(this.label);
-		this.pane.add(this.text);
+		this.pane.add(this.textLogin);
 		this.pane.add(this.button);
 
 		setResizable(true);
@@ -130,7 +135,11 @@ public class Fenetre extends JFrame{
 		label.setAlignmentX(Component.CENTER_ALIGNMENT);
 		this.pane.add(this.label);
 		
-		text = new JTextField(40); 
+		text = new JTextArea(); 
+		text.setRows(3);
+		text.setColumns(40);
+		text.setLineWrap(true);
+		
 		text.setMaximumSize(new Dimension(800,24));
 		text.setMinimumSize(new Dimension(300,24));
 		text.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -196,7 +205,8 @@ public class Fenetre extends JFrame{
 	 * This function adds a message to the JPanel
 	 */
 	public void updateMessage() {
-		JLabel newJLabel = new JLabel();
+		JTextArea newJLabel = new JTextArea(1,40);
+		newJLabel.setWrapStyleWord(true);
 		ImageIcon img = new ImageIcon();
 		JLabel jLabelImg = new JLabel();
 		//System.out.println(checked);
@@ -207,7 +217,45 @@ public class Fenetre extends JFrame{
 			jLabelImg.setIcon(img);
 			if(object.getSujet()!= "OnLineBavardEvent" && object.getSujet()!="OffLineBavardEvent") {
 				System.out.println("titi");
-				newJLabel.setText(object.getBavard().getLogin()+ " says: " + object.getCorps());
+				if(object.getCorps().length()>10) {
+					newJLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+					newJLabel.setText(object.getBavard().getLogin()+ " says: " + object.getCorps().substring(0, 10) + "...");
+					newJLabel.addMouseListener(new MouseListener()  
+					{  
+						@Override
+					    public void mouseClicked(MouseEvent e)  
+					    {  
+							// TODO Auto-generated method stub
+							newJLabel.setText(object.getBavard().getLogin()+ " says: " + object.getCorps());
+					    }
+
+						@Override
+						public void mousePressed(MouseEvent e) {
+							// TODO Auto-generated method stub
+						}
+
+						@Override
+						public void mouseReleased(MouseEvent e) {
+							// TODO Auto-generated method stub
+							
+						}
+
+						@Override
+						public void mouseEntered(MouseEvent e) {
+							// TODO Auto-generated method stub
+							
+						}
+
+						@Override
+						public void mouseExited(MouseEvent e) {
+							// TODO Auto-generated method stub
+							
+						}  
+					}); 
+				}
+				else {
+					newJLabel.setText(object.getBavard().getLogin()+ " says: " + object.getCorps());
+				}
 				newJLabel.setToolTipText("Time : "+ object.getDate()+ " Category : " + object.getSujet());
 				messages.add(jLabelImg);
 				messages.add(newJLabel);
